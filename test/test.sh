7ecd894d7b0bfd074b073d227fe704269d0d1973
imsuneth <imsuneth@gmail.com>
2024-07-16 18:48:31 +1000

    add extensive tests

--- test/test.sh
+++ test/test.sh
@@ -33,36 +33,68 @@ if [ ! -f test/tmp/genome_chr22.fa ]; then
     gzip -d test/tmp/genome_chr22.fa.gz || die "Unzipping the genome chr22 failed"
 fi
 
-echo -e "${BLUE}Test 1: view hifi${NC}"
-ex  ./minimod view -r test/tmp/genome_chr22.fa -m 0.0 test/data/example-hifi.bam > test/tmp/test1.tsv  || die "Test 1: Running the tool failed"
-diff -q test/expected/test1.tsv test/tmp/test1.tsv || die "Test 1: diff failed"
+testname="Test 1: view hifi"
+echo -e "${BLUE}${testname}{NC}"
+ex  ./minimod view -r test/tmp/genome_chr22.fa -m 0.0 -t 8 test/data/example-hifi.bam > test/tmp/test1.tsv  || die "${testname} Running the tool failed"
+diff -q test/expected/test1.tsv test/tmp/test1.tsv || die "${testname} diff failed"
 
-echo -e "${BLUE}Test 2: view ont${NC}"
-ex  ./minimod view -r test/tmp/genome_chr22.fa -m 0.0 test/data/example-ont.bam > test/tmp/test2.tsv || die "Test 2: Running the tool failed"
-diff -q test/expected/test2.tsv test/tmp/test2.tsv || die "Test 2: diff failed"
+testname="Test 2: view ontt"
+echo -e "${BLUE}${testname}${NC}"
+ex  ./minimod view -r test/tmp/genome_chr22.fa -m 0.0 -t 8 test/data/example-ont.bam > test/tmp/test2.tsv || die "${testname} Running the tool failed"
+diff -q test/expected/test2.tsv test/tmp/test2.tsv || die "${testname} diff failed"
 
-echo -e "${BLUE}Test 3: meth-freq hifi${NC}"
-ex  ./minimod meth-freq -r test/tmp/genome_chr22.fa test/data/example-hifi.bam > test/tmp/test3.tsv  || die "Test 3: Running the tool failed"
+testname="Test 3: meth-freq hifi"
+echo -e "${BLUE}${testname}${NC}"
+ex  ./minimod meth-freq -r test/tmp/genome_chr22.fa -t 8 test/data/example-hifi.bam > test/tmp/test3.tsv  || die "${testname} Running the tool failed"
 sort -k1,1 -k2,2n -k4,4 test/expected/test3.tsv > test/expected/test3.tsv.sorted
 sort -k1,1 -k2,2n -k4,4 test/tmp/test3.tsv > test/tmp/test3.tsv.sorted
-diff -q test/expected/test3.tsv.sorted test/tmp/test3.tsv.sorted || die "Test 3: tsv diff failed"
+diff -q test/expected/test3.tsv.sorted test/tmp/test3.tsv.sorted || die "${testname} diff failed"
 
-echo -e "${BLUE}Test 4: meth-freq hifi bedmethyl output${NC}"
-ex  ./minimod meth-freq -b -r test/tmp/genome_chr22.fa test/data/example-hifi.bam > test/tmp/test3.bedmethyl  || die "Test 3: Running the tool failed"
+testname="Test 4: meth-freq hifi bedmethyl output"
+echo -e "${BLUE}${testname}${NC}"
+ex  ./minimod meth-freq -b -r test/tmp/genome_chr22.fa -t 8 test/data/example-hifi.bam > test/tmp/test3.bedmethyl  || die "${testname} Running the tool failed"
 sort -k1,1 -k2,2n -k6,6 test/expected/test3.bedmethyl > test/expected/test3.bedmethyl.sorted
 sort -k1,1 -k2,2n -k6,6 test/tmp/test3.bedmethyl > test/tmp/test3.bedmethyl.sorted
-diff -q test/expected/test3.bedmethyl.sorted test/tmp/test3.bedmethyl.sorted || die "Test 3: bedmethyl diff failed"
+diff -q test/expected/test3.bedmethyl.sorted test/tmp/test3.bedmethyl.sorted || die "${testname} diff failed"
 
-echo -e "${BLUE}Test 5: meth-freq ont${NC}"
-ex  ./minimod meth-freq -r test/tmp/genome_chr22.fa test/data/example-ont.bam > test/tmp/test4.tsv || die "Test 4: Running the tool failed"
+testname="Test 5: meth-freq ont"
+echo -e "${BLUE}${testname}${NC}"
+ex  ./minimod meth-freq -r test/tmp/genome_chr22.fa -t 8 test/data/example-ont.bam > test/tmp/test4.tsv || die "${testname} Running the tool failed"
 sort -k1,1 -k2,2n -k4,4 test/expected/test4.tsv > test/expected/test4.tsv.sorted
 sort -k1,1 -k2,2n -k4,4 test/tmp/test4.tsv > test/tmp/test4.tsv.sorted
-diff -q test/expected/test4.tsv.sorted test/tmp/test4.tsv.sorted || die "Test 4: tsv diff failed"
+diff -q test/expected/test4.tsv.sorted test/tmp/test4.tsv.sorted || die "${testname} diff failed"
 
-echo -e "${BLUE}Test 6: meth-freq ont bedmethyl output${NC}"
-ex  ./minimod meth-freq -b -r test/tmp/genome_chr22.fa test/data/example-ont.bam > test/tmp/test4.bedmethyl || die "Test 4: Running the tool failed"
+testname="Test 6: meth-freq ont bedmethyl output"
+echo -e "${BLUE}${testname}${NC}"
+ex  ./minimod meth-freq -b -r test/tmp/genome_chr22.fa -t 8 test/data/example-ont.bam > test/tmp/test4.bedmethyl || die "${testname} Running the tool failed"
 sort -k1,1 -k2,2n -k6,6 test/expected/test4.bedmethyl > test/expected/test4.bedmethyl.sorted
 sort -k1,1 -k2,2n -k6,6 test/tmp/test4.bedmethyl > test/tmp/test4.bedmethyl.sorted
-diff -q test/expected/test4.bedmethyl.sorted test/tmp/test4.bedmethyl.sorted || die "Test 4: bedmethyl diff failed"
+diff -q test/expected/test4.bedmethyl.sorted test/tmp/test4.bedmethyl.sorted || die "${testname} diff failed"
+
+# ======= Extensive tests (prerequisits: buttery-eel, minimap2) - tested on gtgpu =======
+# blow5=/home/hasindu/scratch/hg2_prom_lsk114_5khz/chr22/PGXXXX230339_reads_chr22.blow5
+# guppybin=/data/suneth/tools/ont-dorado-server/bin
+# model=dna_r10.4.1_e8.2_400bps_5khz_modbases_5hmc_5mc_cg_hac.cfg
+# ref=/genome/hg38noAlt.fa
+# refidx=/genome/hg38noAlt.idx
+
+# buttery-eel -g $guppybin --port 5000 --use_tcp --device cuda:all --call_mods --config $model -i $blow5 -o "reads.unaligned.sam" || die "${testname} Running buttery-eel failed"
+# samtools fastq -@ 32 -TMM,ML "reads.unaligned.sam" | minimap2 -t 32 -x map-ont --sam-hit-only -Y -a -y --secondary=no $refidx - | samtools sort -@ 32 - > "reads.bam" || die "${testname} Running samtools, minimap2 failed"
+# samtools index -@ 32 "reads.bam" || die "${testname} Running samtools index failed"
+
+# testname="Ext. Test 1: view extensive test using PGXXXX230339_reads_chr22"
+# expected=/data/suneth/do_not_delete/ext_test_minimod/ext.test1.tsv
+# echo -e "${BLUE}${testname}${NC}"
+# ex  ./minimod view -r $ref -m 0.0 -t 32 reads.bam > test/tmp/ext.test1.tsv  || die "${testname} Running the tool failed"
+# diff -q $expected test/tmp/ext.test1.tsv || die "${testname} diff failed"
+
+# testname="Ext. Test 2: meth-freq extensive test using PGXXXX230339_reads_chr22"
+# expected=/data/suneth/do_not_delete/ext_test_minimod/ext.test2.tsv
+# echo -e "${BLUE}${testname}${NC}"
+# # ex  ./minimod meth-freq -r $ref -t 32 reads.bam > test/tmp/ext.test2.tsv || die "${testname} Running the tool failed"
+# sort -k1,1 -k2,2n -k4,4 $expected > test/expected/ext.test2.tsv.sorted
+# sort -k1,1 -k2,2n -k4,4 test/tmp/ext.test2.tsv > test/tmp/ext.test2.tsv.sorted
+# diff -q test/expected/ext.test2.tsv.sorted test/tmp/ext.test2.tsv.sorted || die "${testname} diff failed"
+# ============================================================================================
 
 echo -e "${GREEN}ALL TESTS PASSED !${NC}"
